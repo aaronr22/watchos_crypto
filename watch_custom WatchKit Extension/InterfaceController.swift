f3f6a2fbdd2dae00c37907a5e4300e497e667840
Aaron Rotem <aaronrotem@gmail.com>
2017-11-25 20:12:49 -0500

    Initial Commit

---
+++ watch_custom WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watch_custom WatchKit Extension
+//
+//  Created by Aaron Rotem on 11/25/17.
+//  Copyright © 2017 Aaron Rotem. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
